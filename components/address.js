cbb65a2ab1aaaea97305c0c5a873b69f0d96f148
Katherine Huang <hello@katmh.com>
2021-01-02 17:54:54 -0500

    Working address binary explorer

--- components/address.js
+++ components/address.js
@@ -1,53 +1,57 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-const dec2bin = (dec) => {
+const dec2binArray = (dec) => {
   const converted = (dec >>> 0).toString(2);
   return converted.length < 8
-    ? "0".repeat(8 - converted.length) + converted
-    : converted;
+    ? ("0".repeat(8 - converted.length) + converted).split("")
+    : converted.split("");
 };
+const bin2dec = (bin) => parseInt(bin, 2);
 
 const Address = ({ hasError, idyll, updateProps, numbers, ...props }) => {
-  // TODO: human-friendly IP should depend on state too
-  const initialBinNumbers = numbers.map((n) => dec2bin(n));
+  const initialBinNumbers = numbers.map((n) => dec2binArray(n));
   const [binNumbers, setBinNumbers] = useState(initialBinNumbers);
-  const handleChange = (num, idx, e) => {
-    console.log(num, idx, e);
-    // setBinNumbers(binNumbers => ({}))
-  };
-
-  // helper function
-  const updateBinNum = (idx, newVal) => {
-    // setBinNumbers()
+  const handleChange = (binIdx, checkIdx, e) => {
+    const bn = binNumbers.slice();
+    bn[binIdx][checkIdx] = (1 - bn[binIdx][checkIdx]).toString();
+    setBinNumbers(bn);
   };
 
   return (
     <div {...props} className="address_container">
       <div className="address">
-        {numbers.map((n) => (
-          <span key={n} className="number">
-            {n}
-          </span>
-        ))}
+        {binNumbers.map((binNum, i) => {
+          const decNum = bin2dec(binNum.join(""));
+          return (
+            <span key={i} className="number">
+              {decNum}
+            </span>
+          );
+        })}
       </div>
       <div className="binary_address">
-        {binNumbers.map((num, idx) => (
-          <div key={num} className="number_container">
-            <span>{num}</span>
-            <div className="binary_switches">
-              {[...Array(7).keys()].map((k) => (
-                <input
-                  data-number={idx}
-                  key={k}
-                  type="checkbox"
-                  className="binary_switch"
-                  defaultChecked={binNumbers[idx][k].toString().charAt() == 1}
-                  onChange={(e) => handleChange(num, idx, e)}
-                />
-              ))}
+        {binNumbers.map((numArr, binIdx) => {
+          const num = numArr.join("");
+          return (
+            <div key={binIdx} className="number_container">
+              <span>{num}</span>
+              <div className="binary_switches">
+                {[...Array(8).keys()].map((checkIdx) => (
+                  <input
+                    data-number={binIdx}
+                    key={checkIdx}
+                    type="checkbox"
+                    className="binary_switch"
+                    defaultChecked={
+                      binNumbers[binIdx][checkIdx].toString().charAt() == 1
+                    }
+                    onChange={(e) => handleChange(binIdx, checkIdx, e)}
+                  />
+                ))}
+              </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </div>
   );

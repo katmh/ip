b6c2be38d55bb3bc965f16bc8b5a8578a33628c5
Katherine Huang <hello@katmh.com>
2021-01-02 19:46:32 -0500

    Clean address explorer code

--- components/address.js
+++ components/address.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 const dec2binArray = (dec) => {
   const converted = (dec >>> 0).toString(2);
+  // add extra 0s to front if shorter than 8 bits
   return converted.length < 8
     ? ("0".repeat(8 - converted.length) + converted).split("")
     : converted.split("");
@@ -9,8 +10,10 @@ const dec2binArray = (dec) => {
 const bin2dec = (bin) => parseInt(bin, 2);
 
 const Address = ({ hasError, idyll, updateProps, numbers, ...props }) => {
+  // decimal, human-friendly numbers are passed in as numbers prop
   const initialBinNumbers = numbers.map((n) => dec2binArray(n));
   const [binNumbers, setBinNumbers] = useState(initialBinNumbers);
+
   const handleChange = (binIdx, checkIdx, e) => {
     const bn = binNumbers.slice();
     bn[binIdx][checkIdx] = (1 - bn[binIdx][checkIdx]).toString();
@@ -29,16 +32,17 @@ const Address = ({ hasError, idyll, updateProps, numbers, ...props }) => {
           );
         })}
       </div>
+
       <div className="binary_address">
         {binNumbers.map((numArr, binIdx) => {
           const num = numArr.join("");
           return (
             <div key={binIdx} className="number_container">
               <span>{num}</span>
+
               <div className="binary_switches">
                 {[...Array(8).keys()].map((checkIdx) => (
                   <input
-                    data-number={binIdx}
                     key={checkIdx}
                     type="checkbox"
                     className="binary_switch"

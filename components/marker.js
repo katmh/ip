a3632c1a715dfe7f5eed5198601e4fb2d1851ee6
Katherine Huang <hello@katmh.com>
2021-01-04 00:42:26 -0500

    Data on map, custom markers

---
+++ components/marker.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Marker = ({ id }) => {
+  return <div className="marker">{id}</div>;
+};
+
+export default Marker;
--- components/mit-map.js
+++ components/mit-map.js
@@ -1,18 +1,20 @@
 import React, { useEffect, useRef, useState } from "react";
 import mapboxgl from "mapbox-gl";
-// import "mapbox-gl/dist/mapbox-gl.css";
+import buildings from "../data/buildings";
+import SubnetBar from "./subnet-bar";
 
-const MapboxGLMap = () => {
+const MITMap = () => {
   const [map, setMap] = useState(null);
   const mapContainer = useRef(null);
 
   useEffect(() => {
-    mapboxgl.accessToken = process.env.MAPBOX_KEY;
+    mapboxgl.accessToken =
+      "pk.eyJ1Ijoia2F0bWgiLCJhIjoiY2tqaHIwc2g3NG83bjJ5c2I0MzM2cmdpeiJ9.YcnjXWaMrP_-eSnElPXZJA";
     const initializeMap = ({ setMap, mapContainer }) => {
       const map = new mapboxgl.Map({
         container: mapContainer.current,
         style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
-        center: [-71.095, 42.36],
+        center: [-71.092, 42.36],
         zoom: 14,
       });
 
@@ -20,12 +22,33 @@ const MapboxGLMap = () => {
         setMap(map);
         map.resize();
       });
+
+      buildings.forEach((bldg) => {
+        // create a HTML element for each feature
+        var el = document.createElement("div");
+        el.className = "marker";
+        new mapboxgl.Marker(el)
+          .setLngLat([bldg.long_wgs84, bldg.lat_wgs84])
+          .addTo(map);
+      });
     };
 
     if (!map) initializeMap({ setMap, mapContainer });
   }, [map]);
 
-  return <div ref={(el) => (mapContainer.current = el)} />;
+  return (
+    <div
+      style={{
+        padding: "0 5%",
+      }}
+    >
+      <div
+        ref={(el) => (mapContainer.current = el)}
+        style={{ height: "60vh", width: "100%" }}
+      />
+      <SubnetBar />
+    </div>
+  );
 };
 
-export default MapboxGLMap;
+export default MITMap;

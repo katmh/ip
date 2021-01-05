fc80385af3c9ce46e5a1c2d14dcbee5ac653f28e
Katherine Huang <hello@katmh.com>
2021-01-04 22:08:38 -0500

    Start polishing header and binary explorer

---
+++ components/custom-header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const CustomHeader = () => {
+  return (
+    <header>
+      <h1>What's an IP address?</h1>
+      <h1 className="outline">How are they allocated?</h1>
+      <h1>Are we running out?</h1>
+      <p className="byline">
+        By Kat Huang, <i>January 2021</i>
+      </p>
+    </header>
+  );
+};
+
+export default CustomHeader;

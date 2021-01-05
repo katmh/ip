1c6f612538cc1a27cd3788c7a18ac99a5b85331d
Katherine Huang <hello@katmh.com>
2021-01-05 01:10:13 -0500

    Upgrade CIDR explore and classes component

--- components/cidr-explorer.js
+++ components/cidr-explorer.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import Scrabble from "./scrabble";
+import Code from "./code";
+import Latex from "./latex";
 
 const CIDRExplorer = () => {
   const [prefix, setPrefix] = useState(12);
@@ -9,29 +11,49 @@ const CIDRExplorer = () => {
   return (
     <div
       style={{
-        display: "flex",
-        flexWrap: "wrap",
-        justifyContent: "center",
+        textAlign: "center",
+        background: "#eee",
+        padding: "1rem 5% 2rem",
       }}
     >
-      <div
-        className="address"
-        style={{ width: "100%", textAlign: "center", marginBottom: "5rem" }}
-      >
-        <span className="number">18</span>
-        <span className="number">31</span>
-        <span className="number">255</span>
-        <span className="number">255</span>
-        /
-        <input
-          type="number"
-          min="8"
-          max="30"
-          value={prefix}
-          onChange={(e) => handleChange(e)}
-        />
+      <h3>Classless Inter-Domain Routing (CIDR)</h3>
+      <div style={{ display: "flex" }}>
+        <div
+          className="address cidr"
+          style={{ width: "100%", textAlign: "center", marginBottom: "1rem" }}
+        >
+          <span className="number">18</span>
+          <span className="number">31</span>
+          <span className="number">255</span>
+          <span className="number">255</span>
+          /
+          <input
+            type="number"
+            min="8"
+            max="30"
+            value={prefix}
+            onChange={(e) => handleChange(e)}
+            style={{
+              marginLeft: ".25rem",
+              background: "transparent",
+              border: "none",
+              borderBottom: "2px solid #aaa",
+              outline: "none",
+            }}
+          />
+        </div>
+        <p>
+          In CIDR notation, IP addresses are followed by a slash and a number,
+          which tells you how many bits at the beginning represent the network.
+        </p>
       </div>
+
       <Scrabble networkSize={prefix} />
+      <p>
+        The largest blocks of IPs given to organizations are <Code>/8</Code>{" "}
+        (“slash eight”). The first 8 bits represent the network, leaving 24 bits
+        for the host. <Latex>$2^{24}$</Latex> means over 16 million hosts.
+      </p>
     </div>
   );
 };
--- components/classes.js
+++ components/classes.js
@@ -5,14 +5,63 @@ const Classes = () => {
   return (
     <section
       style={{
-        margin: "6rem auto 1rem",
+        margin: "4rem auto",
       }}
     >
-      <Scrabble leading={[0]} networkSize={7} />
-      <Scrabble leading={[1, 0]} networkSize={14} />
-      <Scrabble leading={[1, 1, 0]} networkSize={21} />
-      <Scrabble leading={[1, 1, 1, 0]} networkSize={0} />
-      <Scrabble leading={[1, 1, 1, 1]} networkSize={0} />
+      <div className="class_row">
+        <div className="class_info">
+          <h4>Class A</h4>
+          <p>
+            Many organizations needed more IPs than Class C could provide, but
+            didn’t use nearly as many as Class B offered.
+          </p>
+        </div>
+        <Scrabble leading={[0]} networkSize={7} />
+      </div>
+
+      <div className="class_row">
+        <div className="class_info">
+          <h4>Class B</h4>
+          <p>
+            Many organizations needed more IPs than Class C could provide, but
+            didn’t use nearly as many as Class B offered.
+          </p>
+        </div>
+        <Scrabble leading={[1, 0]} networkSize={14} />
+      </div>
+
+      <div className="class_row">
+        <div className="class_info">
+          <h4>Class C</h4>
+          <p>
+            Many organizations needed more IPs than Class C could provide, but
+            didn’t use nearly as many as Class B offered.
+          </p>
+        </div>
+        <Scrabble leading={[1, 1, 0]} networkSize={21} />
+      </div>
+
+      <div className="class_row">
+        <div className="class_info">
+          <h4>Class D</h4>
+          <p>
+            Many organizations needed more IPs than Class C could provide, but
+            didn’t use nearly as many as Class B offered.
+          </p>
+        </div>
+        <Scrabble leading={[1, 1, 1, 0]} networkSize={0} />
+      </div>
+
+      <div className="class_row">
+        <div className="class_info">
+          <h4>Class E</h4>
+          <p>
+            Many organizations needed more IPs than Class C could provide, but
+            didn’t use nearly as many as Class B offered.
+          </p>
+        </div>
+        <Scrabble leading={[1, 1, 1, 1]} networkSize={0} />
+      </div>
     </section>
   );
 };
